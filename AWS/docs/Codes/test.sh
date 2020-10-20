dda20c52a5467ea9d64508341c9e4d3f51fc06b5
abdallahcoptan <abdallah.cisco@gmail.com>
2020-10-20 21:09:34 +0200

    test web
    Signed-off-by: abdallahcoptan <abdallah.cisco@gmail.com>

---
+++ AWS/docs/Codes/test.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/WillKoehrsen/recurrent-neural-networks.git
+sudo apt-get update
+sudo apt-get install python3-pip
+cd recurrent-neural-networks
+pip3 install --user -r requirements.txt
+cd deployment
+sudo python3 run_keras_server.py
