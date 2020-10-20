721b293eab1c985d7b3cbe3e02ae48eb0d1000fe
abdallahcoptan <abdallah.cisco@gmail.com>
2020-10-21 00:09:11 +0200

    update test
    Signed-off-by: abdallahcoptan <abdallah.cisco@gmail.com>

--- AWS/docs/Codes/test.sh
+++ AWS/docs/Codes/test.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 git clone https://github.com/WillKoehrsen/recurrent-neural-networks.git
-sudo apt-get update
+echo "Y" | sudo apt-get update
 sudo apt-get install python3-pip
 cd recurrent-neural-networks
 pip3 install --user -r requirements.txt
