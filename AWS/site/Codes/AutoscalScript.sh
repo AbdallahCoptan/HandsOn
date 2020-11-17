c2205cafdf8851a481753eae20af97b6f6446706
abdallahcoptan <abdallah.cisco@gmail.com>
2020-11-17 15:27:07 +0100

    complete slides
    Signed-off-by: abdallahcoptan <abdallah.cisco@gmail.com>

---
+++ AWS/site/Codes/AutoscalScript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Y" | sudo apt-get update
+sudo apt install stress
+echo "" | sudo add-apt-repository ppa:ondrej/php
+echo "Y" | sudo apt update
+echo "Y" | sudo apt install php7.2
+sudo groupadd apache
+sudo usermod -a -G apache ubuntu
+sudo chown -R ubuntu:apache /var/www
+sudo chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+cd /var/www/html/
+wget https://raw.githubusercontent.com/AbdallahCoptan/HandsOn/master/AWS/docs/Codes/Load.php
+
